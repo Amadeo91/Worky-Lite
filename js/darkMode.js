005dc6b9a02dcf72575741d23c654ad3452865e1
Amadeusz Olejnik <amadeusz.olejnik66@gmail.com>
2021-06-07 20:46:07 +0200

    edit darkMode script

--- js/darkMode.js
+++ js/darkMode.js
@@ -4,23 +4,33 @@ const h2Tags = document.querySelectorAll('h2');
 const h3Tags = document.querySelectorAll('h3');
 const h4Tags = document.querySelectorAll('h4');
 const pTags = document.querySelectorAll('p');
+const pricingOptions = document.querySelectorAll('pricing__option');
+
 
 const slide = () => {
-    slider.classList.toggle('darkMode__circle--slide')
-    body.classList.toggle('wrapper__dark')
+    slider.classList.toggle('darkMode__circle--slide');
+    body.classList.toggle('wrapper__dark');
 
     h2Tags.forEach(h2Tag => {
         h2Tag.classList.toggle('whiteText')
     });
     h3Tags.forEach(h3Tag => {
-        h3Tag.classList.toggle('whiteText')
+        if (h3Tag.classList.contains('option--js')) {
+            return
+        } else {
+            h3Tag.classList.toggle('whiteText')
+        }
     });
     h4Tags.forEach(h4Tag => {
         h4Tag.classList.toggle('whiteText')
     });
     pTags.forEach(pTag => {
-        pTag.classList.toggle('whiteText')
+        if (pTag.classList.contains('option--js')) {
+            return
+        } else {
+            pTag.classList.toggle('whiteText')
+        }
     });
-}
 
+}
 slider.addEventListener('click', slide);
